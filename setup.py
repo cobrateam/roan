c109eb1c108c3c38a73efdf2f9d9f0cd103fbe3a
Francisco Souza <f@souza.cc>
2011-11-09 22:45:15 -0200

    Changed version and fixed requests requirement

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ with open("README.rst") as f:
 
 setuptools.setup(
     name='roan',
-    version='0.0.1',
+    version='0.1',
     description='Django per-model cache purging',
     long_description=README,
     author='CobraTeam',
     author_email='francisco@franciscosouza.net',
     packages=['roan'],
     include_package_data=True,
-    install_requires=['Django>=1.2', 'request==0.7.6'],
+    install_requires=['Django>=1.2', 'requests==0.7.6'],
 )
