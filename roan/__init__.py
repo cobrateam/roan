073b01e803e2e17c9d1e25aeb10578bf6e4effcf
Francisco Souza <f@souza.cc>
2011-11-09 23:16:30 -0200

    Implemented purging on post_save signal

--- roan/__init__.py
+++ roan/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import requests
 
+from django.conf import settings
 from django.db.models import signals
 
 
@@ -7,9 +9,12 @@ class purge(object):
 
     def __init__(self, url):
         self.url = url
+        self.purge_url = getattr(settings, "ROAN_PURGE_URL", "http://localhost/purge").rstrip("/")
+        self.requests = requests
 
     def on_save(self, model):
         def purger(sender, **kw):
-            pass
+            purge_url = self.purge_url + self.url
+            self.requests.get(purge_url)
 
         signals.post_save.connect(purger, sender=model, weak=False, dispatch_uid='purge_%s_for_%s' % (self.url, model._meta.verbose_name))
--- roan/tests/test_purge.py
+++ roan/tests/test_purge.py
@@ -2,10 +2,13 @@
 import unittest
 import weakref
 
+import requests
 import roan
 
+from django.conf import settings
 from django.db.models import signals
 from polls import models
+from roan.tests import mocks
 
 
 class PurgeTestCase(unittest.TestCase):
@@ -14,8 +17,35 @@ class PurgeTestCase(unittest.TestCase):
         self.bkp_receivers = signals.post_save.receivers
         signals.post_save.receivers = []
 
+        self.old_purge_url = getattr(settings, 'ROAN_PURGE_URL', None)
+        if self.old_purge_url:
+            del settings.ROAN_PURGE_URL
+
     def tearDown(self):
         signals.post_save.receivers = self.bkp_receivers
+
+        if self.old_purge_url:
+            settings.ROAN_PURGE_URL = self.old_purge_url
+        elif hasattr(settings, 'ROAN_PURGE_URL'):
+            del settings.ROAN_PURGE_URL
+
+    def test_should_store_the_requests_module_in_the_local_object(self):
+        p = roan.purge("/")
+        self.assertEquals(requests, p.requests)
+
+    def test_should_store_the_ROAN_PURGE_URL_setting_in_the_object(self):
+        settings.ROAN_PURGE_URL = "http://google.com.br/purge"
+        p = roan.purge("/")
+        self.assertEquals("http://google.com.br/purge", p.purge_url)
+
+    def test_should_remove_the_leading_slash_in_the_ROAN_PURGE_URL_if_its_present(self):
+        settings.ROAN_PURGE_URL = "http://google.com.br/purge/"
+        p = roan.purge("/")
+        self.assertEquals("http://google.com.br/purge", p.purge_url)
+
+    def test_should_use_default_purge_url_when_ROAN_PURGE_URL_is_not_present(self):
+        p = roan.purge("/")
+        self.assertEquals("http://localhost/purge", p.purge_url)
 
     def test_purge_should_receive_and_store_url(self):
         p = roan.purge("/")
@@ -27,8 +57,17 @@ class PurgeTestCase(unittest.TestCase):
         uuids = [r[0][0] for r in signals.post_save.receivers]
         self.assertIn(expected_uid, uuids)
 
-    def test_purge_should_connect_to_save_signal_withou_a_weak_reference(self):
+    def test_purge_should_connect_to_save_signal_without_a_weak_reference(self):
         uid = "purge_/_for_%s" % models.Poll._meta.verbose_name
         roan.purge("/").on_save(models.Poll)
         f = [r[1] for r in signals.post_save.receivers if r[0][0] == uid][0]
         self.assertNotIsInstance(f, weakref.ref)
+
+    def test_when_the_model_is_saved_the_url_should_be_purged(self):
+        purge_url = "http://localhost/purge/polls"
+        p = roan.purge("/polls")
+        p.requests = mocks.RequestsMock(200, purge_url)
+        p.on_save(models.Poll)
+
+        models.Poll.objects.create(title=u'Do you think Roan works well?')
+        self.assertTrue(p.requests.done)
